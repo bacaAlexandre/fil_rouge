8673aba30de0cb0119d1ed20fb6ba893b00968dc
alexandre <Alexandre76b@aol.com>
2019-01-25 15:40:12 +0100

    ajout bundle inirest

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alexa
- * Date: 21/01/2019
- * Time: 11:18
- */
 
 namespace App\Controller;
 
@@ -13,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Unirest;
 
 class DefaultController extends AbstractController
 {
@@ -20,7 +15,17 @@ class DefaultController extends AbstractController
     /**
      * @Route("/accueil", name="homepage")
      */
-    public function indexAction(){
+    public function indexAction()
+    {
+        Unirest\Request::verifyPeer(false);
+        $header = array('Accept' => 'application/json');
+        $body =array(
+            'api_key'=> getenv('API_KEY'),
+            'language'=> getenv('API_LANG'));
+
+        $reponse = Unirest\Request::get('https://api.themoviedb.org/3/trending/movie/day',$header,$body);
+
+        dump($reponse->body);
         return $this->render('default/accueil.html.twig');
     }
 }
